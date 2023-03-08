ae27a17d17a1a3f96897e59fe60e1eddcbb22892
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-03-08 22:14:16 +0100

    added extension

---
+++ src/test/java/logikcode/springframework/petty/services/springdatajpa/PetTypeSDJpaServiceITTest.java
@@ -0,0 +1,42 @@
+package logikcode.springframework.petty.services.springdatajpa;
+
+import logikcode.springframework.petty.junitextensions.TimingExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(TimingExtension.class)
+class PetTypeSDJpaServiceITTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findAll() {
+
+    }
+
+    @Test
+    void findById() {
+
+    }
+
+    @Test
+    void save() {
+
+    }
+
+    @Test
+    void delete() {
+
+    }
+
+    @Test
+    void deleteById() {
+
+    }
+}
